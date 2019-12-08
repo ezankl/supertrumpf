61e0ba23dd11af33570d53895e4ce9b3e41b60b3
ezan <elias.zankl@gmx.de>
2019-12-08 22:58:30 +0100

    Dynamic Card

--- src/Card.jsx
+++ src/Card.jsx
@@ -16,27 +16,18 @@ export default function Card() {
       />
       <table>
         <tbody>
-          <tr>
-            <td>Größe</td>
-            <td>{elephant.size} m</td>
-          </tr>
-          <tr>
-            <td>Gewicht</td>
-            <td>{elephant.weight} kg</td>
-          </tr>
-          <tr>
-            <td>Alter</td>
-            <td>{elephant.age} Jahre</td>
-          </tr>
-          <tr></tr>
-          <tr>
-            <td>Nachkommen</td>
-            <td>{elephant.offspring}</td>
-          </tr>
-          <tr>
-            <td>Geschwindigkeit</td>
-            <td>{elephant.speed} km/h</td>
-          </tr>
+          {Object.keys(Animal.properties).map(property => {
+            const animalProperty = Animal.properties[property];
+            return (
+              <tr key={property}>
+                <td>{animalProperty.label}</td>
+                <td>
+                  {elephant[property]}&nbsp;
+                  {animalProperty.unit}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
