434faba30c594ed3d8fab9acdc7697717912b349
ezan <elias.zankl@gmx.de>
2020-01-01 16:21:44 +0100

    render props, HOC

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 
 import './App.css';
-import Game from './Game';
+import Game from './Game.container';
+import DealCards from './DealCards';
 
-export default function App() {
-  return <Game title="Supertrumpf" />;
+export default class Error extends React.Component {
+  state = {
+    error: null
+  };
+
+  componentDidCatch(error, info) {
+    console.log(error, info);
+  }
+
+  static getDerivedStateFromError(error) {
+    return {
+      error: error.message
+    };
+  }
+
+  render() {
+    if (this.state.error) {
+      return <div>Ein Fehler ist aufgetreten: {this.state.error}</div>;
+    } else {
+      return (
+        <DealCards>
+          {(computer, player) => <Game computer={computer} player={player} title="Supertrumpf" />}
+        </DealCards>
+      );
+    }
+  }
 }
